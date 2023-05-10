25405536c2943009a9c6b7733cdb479722fec47b
JostTrappmann <94616625+JostTrappmann@users.noreply.github.com>
2023-05-10 12:35:00 +0200

    Server Test

--- hebk.de/src/Java/Server.java
+++ hebk.de/src/Java/Server.java
@@ -15,7 +15,7 @@ public class Server {
         ServerSocket serverSock=new ServerSocket(6066);
         Socket Sock=serverSock .accept();
         DataOutputStream out =new DataOutputStream(Sock.getOutputStream());
-        out.writeUTF("i am fine, thank you");
+        out.writeUTF(JOptionPane.showInputDialog("Message"));
         DataInputStream in= new DataInputStream(Sock.getInputStream());
         System.out.println(in.readUTF());
         Sock.close();
