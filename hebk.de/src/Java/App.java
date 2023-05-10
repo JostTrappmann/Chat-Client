a641f3b44f99cc6d6e2aeb020b19539f096138ac
JostTrappmann <94616625+JostTrappmann@users.noreply.github.com>
2023-05-10 12:27:30 +0200

    dd

--- hebk.de/src/Java/App.java
+++ hebk.de/src/Java/App.java
@@ -2,11 +2,12 @@ package hebk.de.src.Java;
 
 import hebk.de.src.Java.Administration;
 
+import java.io.IOException;
 
 
 public class App {
-    public static void main(String [] args)  {
+    public static void main(String [] args) throws IOException {
         Administration administration = new Administration();
-        //administration.serverOrClient();
+        administration.serverOrClient();
     }
 }
--- hebk.de/src/Java/Server.java
+++ hebk.de/src/Java/Server.java
@@ -9,6 +9,7 @@ import javax.swing.JOptionPane;
 
 public class Server {
     public void newSession() throws IOException{
+
         int Port =Integer.parseInt(JOptionPane.showInputDialog("Input Your Port : "));
         String IP = JOptionPane.showInputDialog("Input Your IP Server : ");
         ServerSocket serverSock=new ServerSocket(6066);
